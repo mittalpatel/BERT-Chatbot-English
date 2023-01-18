08faca97eec4fc2d7482bc65e930766e5e7a3feb
mittalpatel <letstalk@pragnakalp.com>
2023-01-18 13:44:48 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# BERT-Chatbot-English
+
+## Instructions to set up the Bert Chatbot English Demo on Local system
+
+1. Clone the repository in your system.
+2. Install and create a virtual environment by the following step. You'll need <b>python <= 3.7</b> to set it up.
+```shell
+sudo apt install virtualenv
+virtualenv venv --python=python3.7
+source venv/bin/activate
+```
+3. Install the following dependencies
+```shell
+pip install Flask
+pip install boto3
+pip install requests
+pip install regex
+pip install flask_cors
+pip install numpy
+pip install transformers==2.1.1
+pip install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+[ Note : above torch command is for python3.7 and no_cuda, you can use this https://pytorch.org/ site to select the torch version according to our python-version and cuda version if you have ]
+```
+4. Now run the app.py by the following command 
+```shell
+python app.py
+```
+5. You can access the chatbot at  http://127.0.0.1:5000/ 
+
+If you have any other questions or face issues in setting the demo up then kindly reach out to us at letstalk@pragnakalp.com 
+
+For all your Natural Language Processing (NLP) requirements, we are here to help you. Email us your requirement at letstalk@pragnakalp.com and don't forget to check out more interesting <a href="https://www.pragnakalp.com/services/natural-language-processing-services/" target="_blank">NLP services</a> we are offering. 
---
+++ app.py
@@ -0,0 +1,78 @@
+# ==============================================================================
+# title              : app.py
+# description        : This is the flask app for Bert closed domain chatbot which accepts the user request and response back with the answer
+# author             : Pragnakalp Techlabs
+# email              : letstalk@pragnakalp.com
+# website            : https://www.pragnakalp.com
+# python_version     : 3.6.x +
+# ==============================================================================
+
+# Import required libraries
+from flask import Flask, render_template, request
+from flask_cors import CORS
+import email
+import csv
+import datetime
+import smtplib
+import ssl
+import socket
+from email.mime.text import MIMEText
+from bert import QA
+
+timestamp = datetime.datetime.now()
+date = timestamp.strftime('%d-%m-%Y')
+time = timestamp.strftime('%I:%M:%S')
+IP = ''
+
+app = Flask(__name__)
+CORS(app)
+
+# Provide the fine_tuned model path in QA Class
+model_en = QA("english_model_bin")
+
+# This is used to show the home page
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# This is used to give response 
+@app.route("/predict")
+def get_bot_response():   
+    IP = request.remote_addr
+    q = request.args.get('msg')
+    bert_bot_log = []
+    bert_bot_log.append(q)
+    bert_bot_log.append(date)
+    bert_bot_log.append(time)
+    bert_bot_log.append(IP)
+    
+    # You can provide your own paragraph from here
+    english_para = "Google was founded in 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its share and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a privately held company on September 4, 1998. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet leading subsidiary and will continue to be the umbrella company for Alphabets Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet."
+
+    # This function creates a log file which contain the question, answer, date, time, IP addr of the user
+    def bert_log_fn(answer_err):
+        bert_bot_log.append(answer_err)
+        with open('bert_bot_log.csv', 'a') as logs:
+            write = csv.writer(logs)
+            write.writerow(bert_bot_log)
+        logs.close()
+
+    # This block calls the prediction function and return the response
+    try:        
+        out = model_en.predict(english_para, q)
+        confidence = out["confidence"]
+        confidence_score = round(confidence*100)
+        if confidence_score > 10:
+            bert_log_fn(out["answer"])
+            return out["answer"]
+        else:
+            bert_log_fn("Sorry I don't know the answer, please try some different question.")
+            return "Sorry I don't know the answer, please try some different question."         
+    except Exception as e:
+        bert_log_fn("Sorry, Server doesn't respond..!!")
+        print("Exception Message ==> ",e)
+        return "Sorry, Server doesn't respond..!!"
+
+# You can change the Flask app port number from here.
+if __name__ == "__main__":
+    app.run(port=5095)
